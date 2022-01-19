32cc73a5147534cd505ce202a97a555718357440
thinkiewinky <boudreault.francis@gmail.com>
2022-01-18 20:06:41 -0500

    creation des logiques pour le fetch sur la DB + login implementation + creation du modifier de format pour le query sur la date

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    NASA_API_KEY: process.env.NASA_API_KEY,
+
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH: process.env.FIREBASE_AUTH,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGEBUCKKET: process.env.FIREBASE_STORAGEBUCKKET,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID
+  }
 }
